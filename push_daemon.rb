bef3298f18d6dfd996d60646e0a7ae8853ffe400
Chris Marshall <chris@chrismar035.com>
2013-06-11 11:53:20 -0400

    Replace Method with Method Object

---
+++ push_daemon.rb
@@ -0,0 +1,33 @@
+require './worker'
+require './udp_server'
+
+class PushDaemon
+  def initialize
+    @worker = Worker.new
+    @server = UDPServer.new
+  end
+
+  def start
+    @worker.spawn(10)
+    @server.bind(6889)
+    loop { process_request }
+  end
+
+  private
+
+    def process_request
+      data = @server.receive
+      case data[0].split.first
+      when 'PING'
+        @server.send('PONG', data[1][3], data[1][1])
+      when 'SEND'
+        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+        json = JSON.generate({
+          "registration_ids" => [$1],
+          "data" => { "alert" => $2 }
+        })
+        @worker << json
+      end
+    end
+end
+
--- pushd.rb
+++ pushd.rb
@@ -2,34 +2,43 @@ require 'thread'
 require 'httpclient'
 require 'json'
 
-queue = Queue.new
-client = HTTPClient.new
-socket = UDPSocket.new
+class PushDaemon
+  def initialize
+    @queue = Queue.new
+    @client = HTTPClient.new
+    @socket = UDPSocket.new
+  end
 
-10.times do
-  Thread.new do
-    while true
-      data = queue.pop
-      client.post('https://android.googleapis.com/send', data, {
-        "Authorization" => "key:AIzaSyCABSTd47XeIH",
-        "Content-Type" => "application/josn"
-      })
+  def start
+    10.times do
+      Thread.new do
+        while true
+          data = @queue.pop
+          @client.post('https://android.googleapis.com/send', data, {
+            "Authorization" => "key:AIzaSyCABSTd47XeIH",
+            "Content-Type" => "application/josn"
+          })
+        end
+      end
+    end
+
+    @socket.bind("0.0.0.0", 6889)
+
+    while data = @socket.recvfrom(4096)
+      case data[0].split.first
+      when 'PING'
+        @socket.send('PONG', 0, data[1][3], data[1][1])
+      when 'SEND'
+        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+        json = JSON.generate({
+          "registration_ids" => [$1],
+          "data" => { "alert" => $2 }
+        })
+        @queue << json
+      end
     end
   end
 end
 
-socket.bind("0.0.0.0", 6889)
-
-while data = socket.recvfrom(4096)
-  case data[0].split.first
-  when 'PING'
-    socket.send('PONG', 0, data[1][3], data[1][1])
-  when 'SEND'
-    data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-    json = JSON.generate({
-      "registration_ids" => [$1],
-      "data" => { "alert" => $2 }
-    })
-    queue << json
-  end
-end
+daemon = PushDaemon.new
+daemon.start
