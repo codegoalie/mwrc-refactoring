605b6225f49c814e08ef6e77499bbf9bdeb35369
Chris Marshall <chris@chrismar035.com>
2013-06-11 11:56:53 -0400

    Extract Method

--- pushd.rb
+++ pushd.rb
@@ -10,34 +10,46 @@ class PushDaemon
   end
 
   def start
-    10.times do
-      Thread.new do
-        while true
-          data = @queue.pop
-          @client.post('https://android.googleapis.com/send', data, {
-            "Authorization" => "key:AIzaSyCABSTd47XeIH",
-            "Content-Type" => "application/josn"
-          })
+    spawn_workers
+    bind
+    process_request
+  end
+
+  private
+
+    def spawn_workers
+      10.times do
+        Thread.new do
+          while true
+            data = @queue.pop
+            @client.post('https://android.googleapis.com/send', data, {
+              "Authorization" => "key:AIzaSyCABSTd47XeIH",
+              "Content-Type" => "application/josn"
+            })
+          end
         end
       end
     end
 
-    @socket.bind("0.0.0.0", 6889)
+    def bind
+      @socket.bind("0.0.0.0", 6889)
+    end
 
-    while data = @socket.recvfrom(4096)
-      case data[0].split.first
-      when 'PING'
-        @socket.send('PONG', 0, data[1][3], data[1][1])
-      when 'SEND'
-        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-        json = JSON.generate({
-          "registration_ids" => [$1],
-          "data" => { "alert" => $2 }
-        })
-        @queue << json
+    def process_request
+      while data = @socket.recvfrom(4096)
+        case data[0].split.first
+        when 'PING'
+          @socket.send('PONG', 0, data[1][3], data[1][1])
+        when 'SEND'
+          data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+          json = JSON.generate({
+            "registration_ids" => [$1],
+            "data" => { "alert" => $2 }
+          })
+          @queue << json
+        end
       end
     end
-  end
 end
 
 daemon = PushDaemon.new
